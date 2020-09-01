f0fad052abbf4ef570db3732ffe7ba928188ef96
vatepri <vatepri@yandex.ru>
2020-09-01 10:33:29 +0300

    add first file

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import '../styles/App.css';
+
+const request = new XMLHttpRequest();
+const baseURL = 'https://www.potterapi.com/v1/';
+const keyURL = '$2a$10$dSooM7l5aj6uLNFOmwf/SObKzKhMgFSrbie2BUTrRmz5hw/jj6Wme';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <div className="content">123</div>
+    </div>
+  );
+};
